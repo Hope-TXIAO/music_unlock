5bf7f067fbb453caf16352f29226fb77f8b22bbf
anchoretqg <betrayerxiao@aliyun.com>
2021-02-28 00:10:33 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.e498f880d9949ac19f2793493054d05b.js"
+  "precache-manifest.4089eb90a20a50fc021e5f61ce09db71.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "unlock-music"});
